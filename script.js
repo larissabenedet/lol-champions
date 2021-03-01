1c6183d3e6db0edebb80ca84008e4439d988bbb1
larissabenedet <larissavb2014@outlook.com>
2021-02-28 22:00:12 -0300

    conteudo centralizado, att na barra de pesquisa e função para fechar o modal quando se clica fora dele

--- script.js
+++ script.js
@@ -2,134 +2,130 @@ let allChampionsName = []
 let allChampionsData = []
 const searchBar = document.getElementById('searchBar')
 
-        async function getContent() {
+async function getContent() {
 
-            try {
-                const response = await fetch('https://ddragon.leagueoflegends.com/cdn/11.2.1/data/pt_BR/champion.json')
+    try {
+        const response = await fetch('https://ddragon.leagueoflegends.com/cdn/11.2.1/data/pt_BR/champion.json')
 
-                var allChampions = await response.json(response)
-                allChampionsName = Object.keys(allChampions.data) // all champions names
-                allChampionsData = Array(allChampions.data) // champion information
+        var allChampions = await response.json(response)
+        allChampionsName = Object.keys(allChampions.data) // all champions names
+        allChampionsData = Array(allChampions.data) // champions info
 
-                showChampions(allChampionsName, allChampionsData)
-            }
+        showChampions(allChampionsName, allChampionsData)
+    }
 
-            catch (error) {
-                console.log(error)
-            }
+    catch (error) {
+        console.log(error)
+    }
 
-        }
+}
 
-        // search
+// search
 
-        searchBar.addEventListener('keyup', (e) => {
-            const searchString = e.target.value.toLowerCase()
-            const result = allChampionsName.filter((e) => {
-                return e.toLowerCase().includes(searchString)
-            })
-            showChampions(result, allChampionsData)
-        })
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase()
+    const result = allChampionsName.filter((e) => {
+        return e.toLowerCase().includes(searchString)
+    })
+    showChampions(result, allChampionsData)
+})
 
-        // div w/ champion and img
+// div w/ champion and img
 
-        function showChampions(allChampionsName, allChampionsData) {
+function showChampions(allChampionsName, allChampionsData) {
 
-            allChampionsData = allChampionsData[0]
+    allChampionsData = allChampionsData[0]
 
-            const htmlString = allChampionsName.map((e, i) => {
-                return `
-                <div class="champ${i}">
+    const htmlString = allChampionsName.map((e, i) => {
+        return `
+                <div class="champ${i} divChamp">
                     <p>${allChampionsData[e].name}</p>
                     <img src="https://ddragon.leagueoflegends.com/cdn/11.4.1/img/champion/${allChampionsData[e].id}.png" class="champImg" alt="${allChampionsData[e].name}">
                 </div>
                 `
-            }).join('')
+    }).join('')
 
-            let container = document.querySelector(`.container`)
-            container.innerHTML = htmlString
-            showButton(allChampionsName, allChampionsData)
+    let container = document.querySelector(`.container`)
+    container.innerHTML = htmlString
+    showButton(allChampionsName, allChampionsData)
+}
+
+// div buttons
+
+function showButton(allChampionsName, allChampionsData) {
+
+    allChampionsName.map((e, i) => {
+        var btInfo = document.createElement("button");
+        btInfo.innerHTML = 'Saiba mais'
+        document.querySelector(`.champ${i}`).appendChild(btInfo);
+        btInfo.className += `btInfo champ${i}`
+        var clickName = allChampionsName[i]
+
+        btInfo.onclick = function () {
+            var clickData = allChampionsData[clickName]
+            var div = document.createElement("div");
+            document.querySelector('main').appendChild(div)
+            div.className += `modalContainer`
+
+            showModal(clickName, clickData)
         }
+    })
+}
 
-        // div buttons
+// modal
 
-        function showButton(allChampionsName, allChampionsData) {
+function showModal(clickName, allChampionsData) {
+    clickName = clickName.toLowerCase()
 
-            allChampionsName.map((e, i) => {
-                var btInfo = document.createElement("button");
-                btInfo.innerHTML = 'Saiba mais'
-                document.querySelector(`.champ${i}`).appendChild(btInfo);
-                btInfo.className += `btInfo champ${i}`
-                var clickName = allChampionsName[i]
-
-                btInfo.onclick = function () {
-                    var clickData = allChampionsData[clickName]
-                    var div = document.createElement("div");
-                    document.querySelector('main').appendChild(div)
-                    div.className += `modalContainer`
-
-                    showModal(clickName, clickData)
-                }
-            })
-        }
-
-        // modal
-
-        function showModal(clickName, allChampionsData) {
-            let div = document.createElement("div");
-            document.querySelector(`.modalContainer`).appendChild(div)
-            div.className += `modal ${clickName}`
-
-            showTagsModal(clickName, allChampionsData)
-        }
-
-        function showTagsModal(clickName, allChampionsData) {
-
-            clickName = clickName.toLowerCase()
-
-            let img = document.createElement("img");
-            img.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${allChampionsData.id}_1.jpg`
-            document.querySelector(`.modal `).appendChild(img);
-            img.className += `champImgModal`
-
-            let pTitle = document.createElement("p");
-            pTitle.innerHTML = `<div>${allChampionsData.name}</div>`
-            document.querySelector(`.modal `).appendChild(pTitle);
-            pTitle.className += 'pTitleModal'
-
-            let pSubtitle = document.createElement("p");
-            pSubtitle.innerHTML = `<div>${allChampionsData.title}</div>`
-            document.querySelector(`.modal `).appendChild(pSubtitle);
-            pSubtitle.className += 'pSubtitleModal'
-
-            let pDescription = document.createElement("p");
-            pDescription.innerHTML = `<div>${allChampionsData.blurb} 
+    let htmlString = `
+            <div class="modal ${clickName}">
+            <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${allChampionsData.id}_0.jpg" alt="${allChampionsData.id}" class="champImgModal">
+            <div><p class="pTitleModal">${allChampionsData.name}</p></div>
+            <div><p class="pSubtitleModal">${allChampionsData.title}</p></div>
+            <div>
+            <p class="pDescriptionModal"> ${allChampionsData.blurb} </p>
             <a class="linkModal" href="https://br.leagueoflegends.com/pt-br/champions/${clickName}/"
-            target="_blank">Continue lendo</a> </div>`
-            document.querySelector(`.modal `).appendChild(pDescription);
-            pDescription.className += 'pDescriptionModal'
+            target="_blank">Continue lendo</a>
+            </div>
+            <span class="spanTagsModal"><div>${allChampionsData.tags.join(' • ')}</div></span>
+            <span class="spanResourceModal"><div>${allChampionsData.partype}</div></span>
+            <button type="button" class="closeButtonModal fas fa-times" onclick="closeButton()"></button>
+            `
 
-            let spanTags = document.createElement("span");
-            spanTags.innerHTML = `<div>${allChampionsData.tags.join(' • ')}</div>`
-            document.querySelector(`.modal `).appendChild(spanTags);
-            spanTags.className += 'spanTagsModal'
+    let modalContainer = document.querySelector(`.modalContainer`)
+    modalContainer.innerHTML = htmlString
 
-            let spanResource = document.createElement("span");
-            spanResource.innerHTML = `<div>${allChampionsData.partype}</div>`
-            document.querySelector(`.modal `).appendChild(spanResource);
-            spanResource.className += 'spanResourceModal'
+    // closes the model when the container is clicked
+    modalContainer.onclick = ((e) => {
+        let modal = document.querySelector('.modal')
+        let main = document.querySelector('main')
+        modalContainer.removeChild(modal)
+        main.removeChild(modalContainer)
+    })
 
-            let closeButton = document.createElement("button");
-            document.querySelector(`.modal `).appendChild(closeButton);
-            closeButton.className += 'closeButtonModal fas fa-times'
+    document.addEventListener('mousemove',disableSearch)
 
-            closeButton.onclick = function () {
-                let modal = document.querySelector('.modal')
-                let modalc = document.querySelector('.modalContainer')
-                let main = document.querySelector('main')
-                modalc.removeChild(modal)
-                main.removeChild(modalc)
+    // disableSearch()
+}
 
-            }
-        }
+function disableSearch() {
+    
+    let modalContainer = document.querySelector('.modalContainer')
 
-        getContent()
+    if (modalContainer != null) {
+        searchBar.style.pointerEvents = 'none'
+    } else {
+        searchBar.style.pointerEvents = 'all'
+    }
+}
+
+function closeButton() {
+
+    let modal = document.querySelector('.modal')
+    let modalc = document.querySelector('.modalContainer')
+    let main = document.querySelector('main')
+    modalc.removeChild(modal)
+    main.removeChild(modalc)
+}
+
+getContent()
